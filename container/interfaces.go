d37434e72913271e63a4cba90b1125f15eaf5e2b
installer <installer@gmail.com>
2025-03-18 22:54:14 +0100

    fixing bugs with wait

--- container/interfaces.go
+++ container/interfaces.go
@@ -1,9 +1,15 @@
 package container
 
+import (
+  "github.com/docker/docker/api/types/container"
+)
+
 type Manager interface {
   Create(config ContainerConfig) (string, error)
   Start(id string) error
   Stop(id string) error
   Remove(id string) error
   GetLogs(id string) (string, error)
+  Wait(id string) (container.WaitResponse, error)
+  IsRunning(id string) (bool, error)
 }

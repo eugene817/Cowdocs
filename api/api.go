1cdb7ad639719dad7db5e529aca1a2c2acde5563
installer <installer@gmail.com>
2025-03-23 13:55:10 +0100

    Added run with channels for goroutines

--- api/api.go
+++ api/api.go
@@ -28,23 +28,38 @@ func (api *API) RunContainer(config container.ContainerConfig) (string, error) {
     if err := api.containerManager.Start(id); err != nil {
         return "", fmt.Errorf("failed to start container: %v", err)
     }
-
-    // running, err := api.containerManager.IsRunning(id)
-    // if err != nil {
-    //     return "", fmt.Errorf("failed to check container status: %w", err)
-    // }
-    // if !running {
-    //     return "", fmt.Errorf("container is not running")
-    // }
-  
+ 
     if _, err := api.containerManager.Wait(id); err != nil { 
         return "", fmt.Errorf("failed to wait for container: %v", err)
     }
     
-        
     logs, err := api.containerManager.GetLogs(id)
     if err != nil {
         return "", fmt.Errorf("failed to get logs: %v", err)
     }
     return logs, nil
+}
+
+
+func (api *API) RunContainerChannel(config container.ContainerConfig, c chan string) error {
+    id, err := api.containerManager.Create(config)
+    if err != nil {
+        return fmt.Errorf("failed to create container: %v", err)
+    }
+    defer api.containerManager.Remove(id)
+
+    if err := api.containerManager.Start(id); err != nil {
+        return fmt.Errorf("failed to start container: %v", err)
+    }
+ 
+    if _, err := api.containerManager.Wait(id); err != nil { 
+        return fmt.Errorf("failed to wait for container: %v", err)
+    }
+    
+    logs, err := api.containerManager.GetLogs(id)
+    if err != nil {
+        return fmt.Errorf("failed to get logs: %v", err)
+    }
+    c <- logs
+    return nil
 }
